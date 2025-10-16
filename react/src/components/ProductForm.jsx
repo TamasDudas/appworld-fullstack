eca26b3627640d3368353093dcdee884c41a9495
Dudás Tamás <ddstms@gmail.com>
2025-10-16 10:48:11 +0200

    Implement product submission in ProductForm with loading and error handling

--- react/src/components/ProductForm.jsx
+++ react/src/components/ProductForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import api from "../api";
 
 export default function ProductForm() {
   const initialProductData = {
@@ -7,14 +8,44 @@ export default function ProductForm() {
   };
 
   const [productData, setProductData] = useState(initialProductData);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   function handleProductData(e) {
     const { name, value } = e.target;
     setProductData((prev) => ({ ...prev, [name]: value }));
   }
 
-  function onSubmit(e) {
+  async function onSubmit(e) {
     e.preventDefault();
+    try {
+      const sendData = {
+        name: productData.name,
+        detail: productData.detail,
+      };
+      const createProductResponse = await api.post("/api/products", sendData);
+      console.log(createProductResponse.data.data.name);
+      setProductData(initialProductData);
+      return { success: true };
+    } catch (error) {
+      return {
+        success: false,
+        error:
+          error?.createProductResponse?.data.data.name ||
+          "Sikertelen termék regisztrálás",
+      };
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  if (loading) {
+    return (
+      <div className="text-center py-8">Termékek hozzáadása folyamatban...</div>
+    );
+  }
+  if (error) {
+    return <div className="text-center py-8 text-red-700">{error}</div>;
   }
   return (
     <form className="space-y-4" onSubmit={onSubmit}>
@@ -40,6 +71,12 @@ export default function ProductForm() {
           className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
         />
       </div>
+      <button
+        className="w-full bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-md transition-colors duration-200 cursor-pointer"
+        type="submit"
+      >
+        Küldés
+      </button>
     </form>
   );
 }

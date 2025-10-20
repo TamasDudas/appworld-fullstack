10d59a66859af7ce6707d166ad29612cc4e37330
Dudás Tamás <ddstms@gmail.com>
2025-10-20 19:19:31 +0200

    Add mobile navigation components for private and public navbars; refactor existing navbar structure for better responsiveness

--- react/src/components/navigation/PrivateNavbar.jsx
+++ react/src/components/navigation/PrivateNavbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import MobilePrivateNavbar from "./MobilePrivateNavbar";
 
 export default function PrivateNavbar() {
   const { user, logout } = useAuth();
@@ -9,40 +10,45 @@ export default function PrivateNavbar() {
   };
 
   return (
-    <nav className="max-w-4xl mx-auto p-4 flex justify-between items-center">
-      {/* Logo */}
-      <Link to="/" className="text-xl font-bold">
-        Appworld Fullstack
-      </Link>
-
-      {/* Nav links */}
-      <div className="flex items-center space-x-4">
-        <Link
-          to="/"
-          className="px-4 py-2 text-blue-600 hover:bg-blue-50 rounded-md"
-        >
-          Home
+    <>
+      <nav className="hidden md:flex max-w-4xl mx-auto p-4  justify-between items-center">
+        {/* Logo */}
+        <Link to="/" className="text-xl font-bold">
+          Appworld Fullstack
         </Link>
-      </div>
-      <div className="flex items-center space-x-4">
-        <Link
-          to="/create-product"
-          className="px-4 py-2 text-blue-600 hover:bg-blue-50 rounded-md"
-        >
-          Termék létrehozása
-        </Link>
-      </div>
 
-      {/* user info */}
-      <div className="flex items-center space-x-4 py-1">
-        <span>Hello, {user?.name}!</span>
-        <button
-          onClick={handleLogout}
-          className="px-2 py-1 text-white bg-red-700 hover:bg-red-900 cursor-pointer rounded-md"
-        >
-          Kilépés
-        </button>
+        {/* Nav links */}
+        <div className="flex items-center space-x-4">
+          <Link
+            to="/"
+            className="px-4 py-2 text-blue-600 hover:bg-blue-50 rounded-md"
+          >
+            Home
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4">
+          <Link
+            to="/create-product"
+            className="px-4 py-2 text-blue-600 hover:bg-blue-50 rounded-md"
+          >
+            Termék létrehozása
+          </Link>
+        </div>
+
+        {/* user info */}
+        <div className="flex items-center space-x-4 py-1">
+          <span>Hello, {user?.name}!</span>
+          <button
+            onClick={handleLogout}
+            className="px-2 py-1 text-white bg-red-700 hover:bg-red-900 cursor-pointer rounded-md"
+          >
+            Kilépés
+          </button>
+        </div>
+      </nav>
+      <div className="md:hidden">
+        <MobilePrivateNavbar user={user} handleLogout={handleLogout} />
       </div>
-    </nav>
+    </>
   );
 }
--- react/src/components/navigation/PublicNavbar.jsx
+++ react/src/components/navigation/PublicNavbar.jsx
@@ -1,39 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import MobilePublicNavbar from "./MobilePublicNavbar";
 
 export default function PublicNavbar() {
   return (
-    <nav className="max-w-4xl mx-auto p-4 flex justify-between items-center">
-      {/* Logo */}
-      <Link to="/" className="text-xl font-bold">
-        Appworld Fullstack
-      </Link>
+    <>
+      <nav className="hidden md:flex max-w-4xl w-full mx-auto p-4 justify-between items-center">
+        {/* Logo */}
+        <Link to="/" className="text-xl font-bold">
+          Appworld Fullstack
+        </Link>
 
-      {/* Nav links */}
-      <div className="flex items-center space-x-4">
-        <Link
-          to="/"
-          className="px-4 py-2 text-blue-600 hover:bg-blue-50 rounded-md"
-        >
-          Home
-        </Link>
+        {/* Nav links */}
+        <div className="flex items-center space-x-4">
+          <Link
+            to="/"
+            className="px-4 py-2 text-blue-600 hover:bg-blue-50 rounded-md"
+          >
+            Home
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4">
+          <Link
+            to="/login"
+            className="px-4 py-2 text-blue-600 hover:bg-blue-50 rounded-md"
+          >
+            Belépés
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4">
+          <Link
+            to="/register"
+            className="px-4 py-2 text-blue-600 hover:bg-blue-50  rounded-md"
+          >
+            Regisztráció
+          </Link>
+        </div>
+      </nav>
+      <div className="md:hidden">
+        <MobilePublicNavbar />
       </div>
-      <div className="flex items-center space-x-4">
-        <Link
-          to="/login"
-          className="px-4 py-2 text-blue-600 hover:bg-blue-50 rounded-md"
-        >
-          Belépés
-        </Link>
-      </div>
-      <div className="flex items-center space-x-4">
-        <Link
-          to="/register"
-          className="px-4 py-2 text-blue-600 hover:bg-blue-50  rounded-md"
-        >
-          Regisztráció
-        </Link>
-      </div>
-    </nav>
+    </>
   );
 }
